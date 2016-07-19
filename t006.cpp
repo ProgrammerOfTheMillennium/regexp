f595f82abb85486b4b40d870e70f6c576173cb68
Bulat Latypov <blatipov@plgn.ru>
2016-07-19 12:33:30 +0500

    Add MeasureUnit class

--- t006.cpp
+++ t006.cpp
@@ -1,4 +1,22 @@
+#include "t006.h"
+
 #include <iostream>
 #include <stdio.h>
+
 #define PATH "statistics_raw_data.txt"
 
+uint8_t* get_data() {
+    FILE *file;
+    file = fopen(PATH, "r");
+
+    size_t fread(void *ptr, size_t size_of_elements,
+                        size_t number_of_elements, FILE *a_file);
+
+    fclose(file);
+    return ptr;
+}
+
+int main(void)
+{
+
+}
--- t006.h
+++ t006.h
@@ -1,4 +1,42 @@
-average()
-mean()
-meadian()
-convert_to_json()
+// void set_data()
+// parth_data()
+// average()
+// mean()
+// meadian()
+// convert_to_json()
+
+#include <iostream>
+
+class MeasureUnit
+{
+    private:
+        std::string date;
+        std::string time;
+        float air_temp;
+        float barometric_press;
+        float dew_point;
+        float relative_humidity;
+        float wind_dir;
+        float wind_gust;
+        float wind_speed;
+    public:
+        void SetDate();
+        void SetTime();
+        void SetAir_Temp();
+        void SetBarometric_Press();
+        void SetDew_Point();
+        void SetRelative_Humidity();
+        void SetWind_Dir();
+        void SetWind_Gust();
+        void SetWind_Speed();
+
+        void GetDate();
+        void GetTime();
+        void GetAir_Temp();
+        void GetBarometric_Press();
+        void GetDew_Point();
+        void GetRelative_Humidity();
+        void GetWind_Dir();
+        void GetWind_Gust();
+        void GetWind_Speed();
+};
