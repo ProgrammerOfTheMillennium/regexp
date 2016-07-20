d3b2b955be8f72ec7896c7019a4f1db88d28cea2
Bulat Latypov <blatipov@plgn.ru>
2016-07-20 10:25:50 +0500

    Add read all needed data to arrays

--- t006.cpp
+++ t006.cpp
@@ -14,16 +14,28 @@ int main(void)
 {
     std::ifstream file(PATH);
     std::string line;
+
+    double air_temp[60195];
+    double bar_press[60195];
+    double wind_speed[60195];
+    int index = 0;
+
+    //Put
     while (std::getline(file, line))
     {
+
         std::istringstream inputString(line);
         boost::xpressive::sregex regge = boost::xpressive::sregex::compile("([0-9]{4}_[0-9]{2}_[0-9]{2})\\s+([0-9]{2}:[0-9]{2}:[0-9]{2})\\s+(\\d+\\.\\d+)\\s+(\\d+\\.\\d+)\\s+(\\d+\\.\\d+)\\s+(\\d+\\.\\d+)\\s+(\\d+\\.\\d+)\\s+(\\d+\\.\\d+)\\s+(\\d+\\.\\d+)");
         boost::xpressive::smatch results;
 
         if(regex_match(line, results, regge))
         {
-            double first_num = std::stod(results[3]);
-            std::cout << first_num << std::endl;
+            air_temp[index] = std::stod(results[3]);
+            bar_press[index] = std::stod(results[4]);
+            wind_speed[index] = std::stod(results[9]);
         }
+        index++;
     }
+
+
 }
